27a30e4b3ca2aa99265dcd8bb3d303d2318ad565
Flower Black <fb2466375479@outlook.com>
2025-03-20 21:49:11 +0800

    `back`: get and load securities. include csi, funds, etf and others.

--- back/src/JQDataTools.py
+++ back/src/JQDataTools.py
@@ -10,10 +10,30 @@ import log
 import os
 import pandas as pd
 
+import structs
 
 class _JQData:
     def __init__(self):
-        self.securities = pd.DataFrame()
+
+        # type -> ( 6-char code -> instrument )
+        self.securities: dict[str, dict[str, structs.InstrumentInfo]] = dict()
+
+    def load(self, raw: pd.DataFrame):
+        self.securities.clear()
+        df = raw
+        for _, row in df.iterrows():
+            inst = structs.InstrumentInfo()
+            inst.id = row.iloc[0].split('.')[0]
+            inst.display_name = row['display_name'] # type: ignore
+            inst.inst_type = row['type'] # type: ignore
+
+            if inst.inst_type not in self.securities:
+                self.securities[inst.inst_type] = dict()
+            
+            self.securities[inst.inst_type][inst.id] = inst
+        
+        log.info('JQ Data loaded.')
+
 
 
 __jqdata = _JQData()
@@ -39,15 +59,37 @@ def __auth() -> bool:
 
 def __get_all_securities():
 
-    cache_file = f'{__cache_dir()}/securities.csv'
-    if os.path.exists(cache_file):
-        __jqdata.securities = pd.read_csv(cache_file)
-        log.info('JQDataTools: securities data loaded from cache.')
+    cache_file_wo_extension = f'{__cache_dir()}/securities'
+    cache_file_parquet = f'{cache_file_wo_extension}.parquet'
+    cache_file_csv = f'{cache_file_wo_extension}.csv'
+
+    securities_raw: pd.DataFrame
+    
+    if os.path.exists(cache_file_parquet):
+        securities_raw = pd.read_parquet(cache_file_parquet, engine='pyarrow')
+        log.info('JQDataTools: securities data loaded from cache (parquet).')
+    elif os.path.exists(cache_file_csv):
+        securities_raw = pd.read_csv(cache_file_csv, encoding='utf-8')
+        log.info('JQDataTools: securities data loaded from cache (csv).')
     else:
-        __jqdata.securities = jqdatasdk.get_all_securities()
-        os.makedirs(__cache_dir())
-        __jqdata.securities.to_csv(cache_file)
-        log.info(f'JQDataTools: secirities saved to {cache_file}')
+        # Cache not found. Just download it.
+
+        securities_raw = jqdatasdk.get_all_securities(
+            types=[
+                'stock', 'fund', 'index', 'futures', 'conbond', 'etf', 'lof', 'fja', 'fjb', 
+                'open_fund', 'bond_fund', 'stock_fund', 'QDII_fund', 'money_market_fund', 
+                'mixture_fund', 'bjse', 'csi'
+            ]
+        ) # type: ignore
+        
+        os.makedirs(__cache_dir(), exist_ok=True)
+        securities_raw.to_parquet(cache_file_parquet, engine='pyarrow')
+        securities_raw.to_csv(cache_file_csv, encoding='utf-8')
+        log.info(f'JQDataTools: secirities saved to {cache_file_parquet}')
+        log.info(f'JQDataTools: secirities saved to {cache_file_csv}')
+    
+    __jqdata.load(securities_raw)
+
 
 
 
--- back/src/structs.py
+++ back/src/structs.py
@@ -6,9 +6,17 @@
     created on 2025.3.20, Shanghai
 """
 
+import enum
 
 class L1MarketData:
     def __init__(self):
         self.instrument_id = 0
         
+
+class InstrumentInfo:
+    def __init__(self):
+        self.id = str()
+        self.display_name = str()
+        self.inst_type = str()
+
 
